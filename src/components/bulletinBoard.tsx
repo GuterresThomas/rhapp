74048e1aa4138672ab34779148f9457935127f56
Guterresthomas <guterresmikael@gmail.com>
2023-08-23 10:17:40 -0300

    bulletinBoard

---
+++ src/components/bulletinBoard.tsx
@@ -0,0 +1,29 @@
+'use client'
+import { useState, useEffect } from "react";
+
+export default function BulletinBoard() {
+    const [bulletin, setBulletin] = useState([])
+    const [newBulletin, setNewBulletin] = useState({
+        title:'',
+        body:'',
+    })
+
+    const fetchBulletins =async () => {
+        const response = await fetch('http://localhost:3000/bulletin_boards')
+        const data = await response.json()
+        setBulletin(data)
+    }
+
+    const addBulletin =async () => {
+        const response = await fetch('http://localhost:3000/bulletin_boards',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newBulletin),
+        })
+
+        
+    }
+}
+
