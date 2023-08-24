9dd1f89c9a207b273bcd8a3e1f6b0cf4e10524b9
Guterresthomas <guterresmikael@gmail.com>
2023-08-24 20:38:10 -0300

    ds

--- src/components/sideBar.tsx
+++ src/components/sideBar.tsx
@@ -104,52 +104,9 @@ function Sidebar() {
                                         </div>
                                     </div>
                                     
-                                    <div className="mt-12 pb-4">
+                                    <div className="mt-12 flex justify-start font-semibold cursor-pointer hover:bg-sky-200 p-4 rounded-xl">
                                         <div className="bg-top bg-cover space-y-1">
-                                            <Link href="#" className="font-medium text-sm items-center rounded-xl text-gray-900 px-4 py-2.5 flex
-                    transition-all duration-200 hover:bg-sky-200 group cursor-pointer">
-                                                <span className="justify-center items-center flex">
-                                                    <span className="justify-center items-center flex">
-                                                        <span className="justify-center items-center flex">
-                                                            <span className="items-center justify-center flex">
-                                                                <svg className="flex-shrink-0 w-5 h-5 mr-4" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                                    viewbox="0 0 24 24" stroke="currentColor" stroke-width="2" /><path stroke-linecap="round"
-                                                                        stroke-linejoin="round" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0
-                              002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756
-                              2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0
-                              00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0
-                              00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0
-                              00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0
-                              001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"/><path
-                                                                    stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016
-                              0z"/>
-                                                            </span>
-                                                        </span>
-                                                    </span>
-                                                </span>
-                                            </Link>
-                                            <Link href="#" className="font-medium text-sm items-center rounded-xl text-gray-900 px-4 py-2.5 flex
-                    transition-all duration-200 hover:bg-sky-200 group cursor-pointer">
-                                                <span className="justify-center items-center flex">
-                                                    <span className="justify-center items-center flex">
-                                                        <span className="justify-center items-center flex">
-                                                            <span className="items-center justify-center flex">
-                                                                <svg className="mr-4" width="24" height="24" viewbox="0 0 24 24" fill="none"
-                                                                    xmlns="http://www.w3.org/2000/svg" /><path d="M8 18.9282C9.21615 19.6303 10.5957 20 12
-                              20C13.4043 20 14.7838 19.6303 16 18.9282C17.2162 18.2261 18.2261 17.2162 18.9282
-                              16C19.6303 14.7838 20 13.4043 20 12C20 10.5957 19.6303 9.21615 18.9282 8C18.2261 6.78385
-                              17.2162 5.77394 16 5.0718C14.7838 4.36965 13.4043 4 12 4C10.5957 4 9.21615 4.36965 8
-                              5.0718" stroke="#4F4F4F" stroke-width="2" /><path d="M2 12L1.21913 11.3753L0.719375
-                              12L1.21913 12.6247L2 12ZM11 13C11.5523 13 12 12.5523 12 12C12 11.4477 11.5523 11 11
-                              11V13ZM5.21913 6.3753L1.21913 11.3753L2.78087 12.6247L6.78087 7.6247L5.21913
-                              6.3753ZM1.21913 12.6247L5.21913 17.6247L6.78087 16.3753L2.78087 11.3753L1.21913 12.6247ZM2
-                              13H11V11H2V13Z" fill="#4F4F4F" />
-                                                            </span>
-                                                        </span>
-                                                    </span>
-                                                </span>
                                                 <ButtonLogOut />
-                                            </Link>
                                         </div>
                                     </div>
                                 </div>
