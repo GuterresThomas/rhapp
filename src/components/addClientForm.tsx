182e908a1fb76db5d4aba6f1e3caff0923a72bed
Guterresthomas <guterresmikael@gmail.com>
2023-08-24 22:03:22 -0300

    as

---
+++ src/components/addClientForm.tsx
@@ -0,0 +1,70 @@
+'use client'
+import { useState, useEffect } from "react";
+import { ScrollArea } from "@/components/ui/scroll-area"
+
+
+export default function AddForm() {
+    const [customers, setCustomers] = useState([])
+    const [newCustomer, setNewCustomer] = useState({
+        name: '',
+        contact_info:'',
+        cpf:'',
+    })
+
+    const fetchCustmores = async () => {
+        const response = await fetch('http://localhost:3000/customers')
+        const data = await response.json()
+        setCustomers(data)
+    }
+
+    const addCustomer =async () => {
+        const response = await fetch('http://localhost:3000/customers', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newCustomer),
+        })
+        if (response.status === 201) {
+            setNewCustomer({
+                name: '',
+                contact_info:'',
+                cpf:'',
+            })
+            fetchCustmores()
+        }     
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        addCustomer();
+        alert('Cliente adicionado')
+        window.location.reload()
+    }   
+    
+    useEffect(() => {
+        fetchCustmores()
+    }, [])
+
+    return (
+        <div className="flex flex-col justify-center  p-4 bg-sky-100 rounded-xl h-screen">
+            <ScrollArea>
+                <div>
+                    <span className="font-bold p-2 m-2">Adicionar cliente:</span>
+                </div>
+                <form onSubmit={handleSubmit} className="flex-col flex justify-center gap-2 p-2 ">
+                    <label htmlFor="name">Nome:</label>
+                    <input className="bg-sky-50 rounded-xl p-1 w-full hover:bg-sky-200" type="text" id="name" name="name" value={newCustomer.name} onChange={(e) => setNewCustomer({ ...newCustomer, name: e.target.value })} />
+                    
+                    <label htmlFor="contact_info">Dados de contato:</label>
+                    <input className="bg-sky-50 rounded-xl p-1 w-full hover:bg-sky-200" type="text" id="contact_info" name="contact_info" value={newCustomer.contact_info} onChange={(e) => setNewCustomer({ ...newCustomer, contact_info: e.target.value })} />
+                    
+                    <label htmlFor="cpf">CPF:</label>
+                    <input className="bg-sky-50 rounded-xl p-1 w-full hover:bg-sky-200" type="text" id="cpf" name="cpf" value={newCustomer.cpf} onChange={(e) => setNewCustomer({ ...newCustomer, cpf: e.target.value })} />
+                    
+                    <button type="submit" className="bg-sky-50 p-3 font-bold rounded-xl hover:bg-sky-200 m-2">Adicionar Cliente</button>
+                </form>
+            </ScrollArea>
+        </div>
+        
+    )
+}
