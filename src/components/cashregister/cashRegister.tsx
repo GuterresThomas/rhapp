cb031a0188b5135c3eb1b50a46be763ad0a51a97
Guterresthomas <guterresmikael@gmail.com>
2023-09-04 17:25:58 -0300

    kos

--- src/components/cashregister/cashRegister.tsx
+++ src/components/cashregister/cashRegister.tsx
@@ -5,6 +5,13 @@ import { useState, useEffect } from "react";
 export default function CashRegister() {
     const [selectedProduct, setSelectedProduct] = useState([])
     const [selectedCustomer, setSelectedCustomer] = useState([])
+    const [paymenteMethod, setPaymenteMethod] = useState('')
+    const [totalAmout, setTotalAmout] = useState(0)
 
-    
+    const fetchProducts =async () => {
+        const response = await fetch('')
+        const data = await response.json()
+        setSelectedProduct(data)
+    }
+
 }
