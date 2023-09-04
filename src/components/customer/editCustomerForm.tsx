fe91fc7b369bf3308f1b75dfebb5a50814f609ee
Guterresthomas <guterresmikael@gmail.com>
2023-09-04 16:17:11 -0300

    ksao

--- src/components/customer/editCustomerForm.tsx
+++ src/components/customer/editCustomerForm.tsx
@@ -44,13 +44,13 @@ function EditCustomerForm({ customer, onUpdate }) {
                 <span className="font-bold p-2 m-2">Editar Cliente:</span>
             </div>
             <form onSubmit={handleSubmit} className="flex-col flex justify-center gap-2 p-2 ">
-                <label htmlFor="name">Nome:</label>
+                <label className="font-semibold" htmlFor="name">Nome:</label>
                 <input className="bg-sky-50 rounded-xl p-1 w-full hover:bg-sky-200" type="text" id="name" name="name" value={editedCustomer.name} onChange={handleInputChange} required />
                 
-                <label htmlFor="contact_info">Dados de contato:</label>
+                <label className="font-semibold" htmlFor="contact_info">Dados de contato:</label>
                 <input className="bg-sky-50 rounded-xl p-1 w-full hover:bg-sky-200" type="text" id="contact_info" name="contact_info" value={editedCustomer.contact_info} onChange={handleInputChange} required />
                 
-                <label htmlFor="cpf">CPF:</label>
+                <label className="font-semibold" htmlFor="cpf">CPF:</label>
                 <input className="bg-sky-50 rounded-xl p-1 w-full hover:bg-sky-200" type="text" id="cpf" name="cpf" value={editedCustomer.cpf} onChange={handleInputChange} required />
                 
                 <button type="submit" className="bg-sky-50 p-3 font-bold rounded-xl hover:bg-sky-200 m-2">Editar Cliente</button>
