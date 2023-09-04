f5fe094023cd05ddc69af823134d4d32010539d6
Guterresthomas <guterresmikael@gmail.com>
2023-09-04 17:03:53 -0300

    mn

---
+++ src/components/cashregister/cashRegister.tsx
@@ -0,0 +1,10 @@
+'use clien'
+import { useState, useEffect } from "react";
+
+
+export default function CashRegister() {
+    const [selectedProduct, setSelectedProduct] = useState([])
+    const [selectedCustomer, setSelectedCustomer] = useState([])
+
+    
+}
