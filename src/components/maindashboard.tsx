0717fab635d53fa0e7a8d727b3587a8db6164f92
Guterresthomas <guterresmikael@gmail.com>
2023-09-05 13:22:13 -0300

    oska

--- src/components/maindashboard.tsx
+++ src/components/maindashboard.tsx
@@ -32,9 +32,76 @@ export default function MainDashboard() {
           </div>
           <Tabs defaultValue="overview" className="space-y-4">
             <TabsList className="shadow-black shadow-sm hover:bg-sky-200 rounded-xl">
-              <TabsTrigger value="overview" className="font-semibold text-lg ">Funcionários</TabsTrigger>
+              <TabsTrigger value="overview" className="font-semibold text-lg m-2 ">Funcionários</TabsTrigger>
+            </TabsList>
+            <TabsList className="shadow-black shadow-sm m-2 hover:bg-sky-200 rounded-xl">
+              <TabsTrigger value="2" className="font-semibold text-lg m-2 ">Vendas</TabsTrigger>
             </TabsList>
             <TabsContent value="overview" className="space-y-4">
+              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+                <Card className="shadow-black shadow-md rounded-xl">
+                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-semibold">
+                      Salários:
+                    </CardTitle>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="h-4 w-4 text-muted-foreground"
+                    >
+                      <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+                    </svg>
+                  </CardHeader>
+                  <CardContent>
+                    <div>
+                      <p className="text-md font-semibold">
+                        <AverageSalaryCalc />
+                      </p>
+                    </div>
+                  </CardContent>
+                </Card>
+                <Card className="shadow-black shadow-md rounded-xl">
+                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-semibold">
+                      Idade:
+                    </CardTitle>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="h-4 w-4 text-muted-foreground"
+                    >
+                      <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+                      <circle cx="9" cy="7" r="4" />
+                      <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+                    </svg>
+                  </CardHeader>
+                  <CardContent>
+                    <p className="text-md font-semibold">
+                      <AverageAgeCalc />
+                    </p>
+                  </CardContent>
+                </Card>
+                <Card className="shadow-black shadow-md rounded-xl">
+                  <CardHeader>
+                    <CardTitle>Distribuição de Genêro entre os funcionários:</CardTitle>
+                  </CardHeader>
+                  <CardContent className="pl-2">
+                    <GenderDistTrue />
+                  </CardContent>
+                </Card>
+              </div>
+            </TabsContent>
+            <TabsContent value="2" className="space-y-4">
               <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
                 <Card className="shadow-black shadow-md rounded-xl">
                   <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
