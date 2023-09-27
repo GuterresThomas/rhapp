244d28b5d35d9f9349f515ebbf61dc5fbe2f8092
Guterresthomas <guterresmikael@gmail.com>
2023-09-27 16:48:36 -0300

    same

--- src/app/bulletin/page.tsx
+++ src/app/bulletin/page.tsx
@@ -1,16 +1,19 @@
 import Sidebar from "@/components/sideBar"
 import BulletinBoard from "@/components/bulletinBoard"
+import UserRouteProtection from "@/components/userRouteProtection"
 
 export default function Page() {
     
     return (
-        <div className="flex  gap-10">
-            <div>
-                <Sidebar />
+        <UserRouteProtection>
+            <div className="flex  gap-10">
+                <div>
+                    <Sidebar />
+                </div>
+                <div className=" w-3/4 justify-center bg-sky-100 m-2 flex">
+                    <BulletinBoard />
+                </div>
             </div>
-            <div className=" w-3/4 justify-center bg-sky-100 m-2 flex">
-                <BulletinBoard />
-            </div>
-        </div>
+        </UserRouteProtection>
     )
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,19 @@
 import Sidebar from "@/components/sideBar"
 import MainDashBoard from "@/components/maindashboard"
+import UserRouteProtection from "@/components/userRouteProtection"
 
 export default function Page() {
     
     return (
-        <div className="flex justify-center gap-10">
-            <div>
-                <Sidebar />
+        <UserRouteProtection>
+            <div className="flex justify-center gap-10">
+                <div>
+                    <Sidebar />
+                </div>
+                <div className="justify-center bg-sky-100 m-2 flex">
+                    <MainDashBoard />
+                </div>
             </div>
-            <div className="justify-center bg-sky-100 m-2 flex">
-                <MainDashBoard />
-            </div>
-        </div>
+        </UserRouteProtection>
     )
 }
